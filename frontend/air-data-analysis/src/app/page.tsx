02efa0e80e03a24ab232c47de93d0082f065a371
Divyanshu Mathpal <divyanshumathpal@gmail.com>
2024-12-10 00:49:33 -0500

    Added skeleton for the web

--- frontend/air-data-analysis/src/app/page.tsx
+++ frontend/air-data-analysis/src/app/page.tsx
@@ -1,95 +1,94 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client"
+import * as React from 'react';
+import {Typography,Box, Stack, Checkbox} from "@mui/material";
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select, { SelectChangeEvent } from '@mui/material/Select';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import { Line, XAxis, YAxis,Tooltip } from 'recharts';
+import dynamic from "next/dynamic"
+
+// Dynamically import LineChart and related components with SSR disabled
+const LineChart = dynamic(() => import('recharts').then((mod) => mod.LineChart), { ssr: false });
+
+const CartesianGrid = dynamic(() => import('recharts').then((mod) => mod.CartesianGrid), { ssr: false });
+
+
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+  const data = [
+    { name: 'Jan', uv: 400, pv: 2400 },
+    { name: 'Feb', uv: 300, pv: 1398 },
+    { name: 'Mar', uv: 200, pv: 9800 },
+    { name: 'Apr', uv: 278, pv: 3908 },
+    { name: 'May', uv: 189, pv: 4800 },
+  ];
+  const [source, setSource] = React.useState<string>("");
+  const [filters,setFilters] = React.useState<string[]>([]);
+
+  const DummyDataSource:Map<string,number> = new Map<string,number>();
+  DummyDataSource.set("flightEcon",0);
+  DummyDataSource.set("GDP",1);
+
+  const Filters:string[][] = [
+    ["filter1_econ","filter2_econ"],
+    ["filter1_GDP","filter2_GDP"],
+  ]
+  const handleSourceChange = (event: SelectChangeEvent) => {
+    setSource(event.target.value as string);
+  };
+  const handleFiltersChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setFilters((filters) => [...filters,event.target.value as string]);
+  }
+   return (
+    <Box
+    sx={{
+      display: 'flex',              // Flexbox to center content
+      flexDirection: 'column',      // Stack elements vertically
+      alignItems: 'center',         // Horizontally center content
+      justifyContent: 'top',     // Vertically center content
+      minHeight: '100vh',           // Full viewport height
+      backgroundColor: '#f0f0f0',   // Light background color
+      padding: 2,                   // Padding around the content
+    }}
+  >
+    <Typography variant="h2" color="black">Air data analysis</Typography>
+    <Typography color="black">
+      Choose a data source:
+    </Typography>
+    <FormControl fullWidth>
+        <InputLabel>Data source</InputLabel>
+        <Select
+          value={source}
+          label="Data Source"
+          onChange={handleSourceChange}
+          fullWidth={true}
         >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+          <MenuItem value={"flightEcon"}>flightEcon</MenuItem>
+          <MenuItem value={"GDP"}>GDP</MenuItem>
+        </Select>
+      </FormControl>
+      <Typography color="black">
+      Choose some filters:
+    </Typography>
+    <Stack direction={"row"}>
+    {source==""?null:
+          Filters[DummyDataSource.get(source) as number].map((value,index) => 
+            (
+              <FormControlLabel key={index} control={<Checkbox />} label={value} sx={{ color: 'black' }}  />
+        ))}
+    </Stack>
+    <LineChart width={600} height={300} data={data}>
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="name" />
+        <YAxis />
+        <Tooltip />
+        <Line type="monotone" dataKey="uv" stroke="#8884d8" activeDot={{ r: 8 }} />
+        <Line type="monotone" dataKey="pv" stroke="#82ca9d" />
+      </LineChart>
+
+  </Box>
   );
 }
