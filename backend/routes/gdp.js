25efdbc448d080e54402f395a659f3a9a07974b1
Divyansh Singh <25680829+mod-1@users.noreply.github.com>
2024-12-11 17:58:43 -0500

    Cleanup

--- backend/routes/gdp.js
+++ backend/routes/gdp.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const gdp = require('../models/gdp');
 const CleanPassengerInfo = require('../models/CleanPassengerInfo');
 
 router.get('/gdp', async (req, res) => {
@@ -18,28 +17,6 @@ router.get('/gdp', async (req, res) => {
     const end = new Date(endDate);
     endYear = end.getFullYear();
     endQuarter = Math.floor((end.getMonth() + 3) / 3);
-  }
-
-  // Build the query object for GDP
-  let gdpQuery = {};
-  if (state) {
-    gdpQuery.Region = state;
-  } else {
-    gdpQuery.Region = 'United States';
-  }
-
-  if (startDate && endDate) {
-    gdpQuery.$or = [
-      { year: { $gt: startYear, $lt: endYear } },
-      { year: startYear, quarter: { $gte: startQuarter } },
-      { year: endYear, quarter: { $lte: endQuarter } }
-    ];
-  } else if (startDate) {
-    gdpQuery.year = { $gte: startYear };
-    gdpQuery.quarter = { $gte: startQuarter };
-  } else if (endDate) {
-    gdpQuery.year = { $lte: endYear };
-    gdpQuery.quarter = { $lte: endQuarter };
   }
 
   // Build the aggregation pipeline for passengers
@@ -61,41 +38,7 @@ router.get('/gdp', async (req, res) => {
     passengerMatch.YEAR = { $lte: endYear };
     passengerMatch.QUARTER = { $lte: endQuarter };
   }
-  console.log("Lets gooo")
    try {
-    // const pipeline = [
-    //   { $match: gdpQuery },
-    //   {
-    //     $lookup: {
-    //       from: 'clean-passenger-info',
-    //       let: { year: '$year', quarter: '$quarter' },
-    //       pipeline: [
-    //         { $match: { $expr: { $and: [
-    //           { $eq: ['$YEAR', '$$year'] },
-    //           { $eq: ['$QUARTER', '$$quarter'] },
-    //           ...(state ? [{ $eq: ['$ORIGIN_STATE_NM', state] }] : [])
-    //         ]}}},
-    //         { $group: {
-    //           _id: null,
-    //           totalPassengers: { $sum: '$PASSENGERS' }
-    //         }}
-    //       ],
-    //       as: 'passengerData'
-    //     }
-    //   },
-    //   { $unwind: { path: '$passengerData', preserveNullAndEmptyArrays: true } },
-    //   {
-    //     $project: {
-    //       year: 1,
-    //       quarter: 1,
-    //       region: '$Region',
-    //       passenger_count: { $ifNull: ['$passengerData.totalPassengers', 0] },
-    //       gdp: '$gdp_value'
-    //     }
-    //   },
-    //   { $sort: { year: 1, quarter: 1 } }
-     // ];
-     
      const pipeline = [
       { $match: passengerMatch },
       {
@@ -135,8 +78,6 @@ router.get('/gdp', async (req, res) => {
     ];
 
     const result = await CleanPassengerInfo.aggregate(pipeline);
-    console.log('Result:', result);
-
     res.json(result);
   } catch (err) {
     console.error('Error:', err);
